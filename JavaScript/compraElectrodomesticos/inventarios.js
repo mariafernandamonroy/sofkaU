092d175cd63dd41de66a09d8b5c579b4a3e6e321
user <user@iMac-de-user-2.local>
2021-09-06 22:28:29 -0500

    Se agrego inventario en un archivo por separado

---
+++ JavaScript/compraElectrodomesticos/inventarios.js
@@ -0,0 +1,43 @@
+export const InventarioTv = {
+    tv: 23,
+
+    tvA: 3,
+    tvANal: 3,
+    tvAInter: 0,
+
+    tvB: 13,
+    tvBNal: 0,
+    tvBInter: 13,
+
+    tvC: 7,
+    tvCNal: 0,
+    tvCInter: 7
+}
+
+export const InventarioNevera = {
+    nev: 22,
+    
+    nevA: 10,
+    nevANal: 10,
+    nevAInter: 0,
+
+    nevB: 0,
+
+    nevC:12,
+    nevCNal: 0,
+    nevCInter: 12
+}
+
+export const InventarioElectrodomestico = {
+    elec: 13,
+
+    elecA:8,
+    elecANal: 0,
+    elecAInter: 8,
+
+    elecB: 5,
+    elecBNAl: 5,
+    elecBInter: 0,
+
+    elecC: 0  
+}
